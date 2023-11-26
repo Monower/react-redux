796f18e37321cd621b4b7bac64973ed4aa83c6ae
monower <sadafahmed720@gmail.com>
2023-11-26 22:51:44 +0600

    minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useSelector } from "react-redux"
+
 
 function App() {
-  const counter = useSelector((state) => state.counter);
+
   return (
     <>
-      <div>
-        <h1>Counter App</h1>
-        { counter }
-      </div>
     </>
   )
 }
